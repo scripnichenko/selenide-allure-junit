d9509f868952f237d365639dc3ceaed5c1c23891
Andrii Skrypnychenko <scrip.a.v@gmail.com>
2017-05-07 00:59:41 +0200

    1st steps with Selenide

--- src/test/java/org/selenide/examples/GoogleSearchTest.java
+++ src/test/java/org/selenide/examples/GoogleSearchTest.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class GoogleSearchTest {
   @Test
   public void userCanSearchAnyKeyword() {
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\.m2\\repository\\org\\seleniumhq\\selenium\\selenium-chrome-driver\\2.53.1");
     open("http://google.com/en");
     $(By.name("q")).val("selenide").pressEnter();
     $$("#ires li.g").shouldHave(size(10));
